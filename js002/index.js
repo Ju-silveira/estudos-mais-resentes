4171497a572d216fb0196b3d84fdb4ffd8a0f5a1
Julio <105402363+Ju-silveira@users.noreply.github.com>
2024-10-29 21:27:33 -0300

    completando a aula dois
    aprendendo variveu e constante

--- js002/index.js
+++ js002/index.js
@@ -4,3 +4,21 @@
 console.log("olá mundo!")
 document.write("olá mundo!")
 alert("olá mundo!")
+
+// variavel pode ser alterada
+let nome = "julio"
+let peso = 60.0
+let idade = 21
+let tavivo = true
+let teste = null
+
+//constrantes nãopode ser alterada
+const PI = 986855
+
+//testando variavel e constante feitas
+document.write(nome)
+document.write(peso)
+document.write(idade)
+document.write(tavivo)
+document.write(teste)
+document.write(PI)
